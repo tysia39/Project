41af0a8a81b4bc2c8a699800a00a4f0416967072
tysia39 <44493010+tysia39@users.noreply.github.com>
2018-11-08 16:08:18 +0100

    Add files via upload

---
+++ zad4.c
@@ -0,0 +1,25 @@
+/*Program prosi o podanie dwch liczb cakowitych: liczba1 i liczba2.
+Jeeli liczba1<=liczba2
+   Program wypisuje liczby parzyste z przedziau <liczba1,liczba2>
+Jeeli liczba1>liczba2
+   Program wypisuje liczby parzyste z przedziau <liczba2,liczba1>*/
+
+
+#include <stdio.h>
+int main()
+{
+    int liczba1,liczba2,i;
+    printf("Podaj liczba 1: ");
+    scanf("%i",&liczba1);
+    {
+        for (i=liczba1;i<=liczba2;i++)
+            if(i%2==0)printf("%3i",i);
+    }
+    else
+    {
+        for (i=liczba2;i<=liczba1;i++)
+            if(i%2==0)printf("%3i",i);
+    }
+    getchar();
+    return 0;
+}
